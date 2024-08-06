de522b143f6b4170f9d2c7b00bf048ab49200e18
Ben <benk@lavatech.ltd.uk>
2024-08-06 23:00:08 +0100

    Paginate index page

--- public/index.php
+++ public/index.php
@@ -1,10 +1,20 @@
 <?php
 $forests = [];
+$resultsPerPage = 30;
+$totalResults = 0;
+$pageNumber = $_GET['page'] ?? 1;
+$limitStartNumber = ($pageNumber - 1) * $resultsPerPage;
 
 try {
     $db = new PDO('sqlite:../database/us-wildfires.sqlite');
 
-    $stmt = $db->prepare("SELECT DISTINCT NWCG_REPORTING_UNIT_NAME FROM fires");
+    $countStmt = $db->prepare("SELECT COUNT(DISTINCT NWCG_REPORTING_UNIT_NAME) FROM fires");
+    $countStmt->execute();
+    $totalResults = $countStmt->fetchColumn();
+
+    $stmt = $db->prepare("SELECT DISTINCT NWCG_REPORTING_UNIT_NAME FROM fires LIMIT :start, :limit");
+    $stmt->bindParam(':start', $limitStartNumber, PDO::PARAM_INT);
+    $stmt->bindParam(':limit', $resultsPerPage, PDO::PARAM_INT);
     $stmt->execute();
 
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
@@ -37,6 +47,19 @@ try {
             <?php endforeach; ?>
         </ul>
     </section>
+    <nav>
+        <?php
+        $totalPages = ceil($totalResults / $resultsPerPage);
+        $prevPage = $pageNumber > 1 ? $pageNumber - 1 : 1;
+        $nextPage = $pageNumber < $totalPages ? $pageNumber + 1 : $totalPages;
+        ?>
+        <a class="pagination" href="?page=1">First</a>
+        <a class="pagination" href="?page=<?= $prevPage ?>">Prev</a>
+        <span class="pagination current-page"><?= $pageNumber ?></span>
+        <a class="pagination" href="?page=<?= $nextPage ?>">Next</a>
+        <a class="pagination" href="?page=<?= $totalPages ?>">Last</a>
+        <span class="pagination current-page">Total: <?= $totalPages ?></span>
+    </nav>
 </main>
 </body>
 </html>
